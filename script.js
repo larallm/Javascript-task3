d357a4ba728ade3a5ccd9a0694fa407b58414e97
Lara Lopes <laralopesmarques@gmail.com>
2022-03-24 13:52:29 -0300

    Terceira task

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = prompt("Digite o seu nome: ");
+alert("seu nome é "+ nome);
+console.log(nome);
+
+let num1 = parseInt(prompt("Escreva um num"));
+let num2 = parseInt(prompt("Escreva outro num"));
+
+console.log(num1+num2);
+
+let nota1 = parseInt(prompt("Escreva um nota"));
+let nota2 = parseInt(prompt("Escreva outra nota"));
+
+console.log((nota1 + nota2)/2);
